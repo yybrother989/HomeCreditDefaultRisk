2d03004920290250d98557a3c98f2e879e85757b
Yue Gao <yue.gao1@outlook.com>
2022-06-25 02:35:27 -0400

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ st.write("""
 # Home Credit Default Risk
 Data Visualization*
 """)
+st.write("""This app is a Streamlit dashboard that shows \
+the data visualization of the Home Credit Default Risk dataset. """)
+
