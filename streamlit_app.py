b1e8e67ae0bf71de68cf6d79db44372f27bc28da
Yue Gao <yue.gao1@outlook.com>
2022-06-30 13:08:57 -0400

    Update streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,25 @@
 import streamlit as st
-st.write("""
-# Home Credit Default Risk
-Data Visualization*
-""")
-st.write("""This app is a Streamlit dashboard that shows \
-the data visualization of the Home Credit Default Risk dataset. """)
+st.title("""Home Credit Default Risk""")
+st.write("""BrainStation Data Science Program Capstone Project""")
+st.write("""Author: Yue Gao""")
+st.write("""Email: yue.gao1@outlook.com\n""")
+st.image("""https://gadgetstouse.com/wp-content/uploads/2016/06/2-logo.jpg""", width=500)
+
+st.header("""Introduction""")
+st.header("""Exploratory Data Analysis""")
+st.subheader("""Missing Values Inspection and Imputation""")
+st.subheader("""Data Visualization""")
+st.header("""Feature Engineering""")
+st.header("""Model Building""")
+st.subheader("""Base Model -- Decision Tree""")
+st.subheader("""Bagging Model -- Random Forest""")
+st.subheader("""Boosting Model -- XGBoost""")
+st.subheader("""Final Model -- LightGBM""")
+st.header("""Credit Fast Approval Application""")
+
+
+
+
+
+
 
